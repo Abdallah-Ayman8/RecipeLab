139b0e70df1b4e3820288c93651ccc471ec5aec9
Abdallah Ayman <Abdallah5919771@gmail.com>
2026-06-19 17:53:21 +0300

    add more width for drop down menu

--- components/Input/DropDownMenu.jsx
+++ components/Input/DropDownMenu.jsx
@@ -15,20 +15,20 @@ export default function DropDownMenu({ items }) {
       <DropdownMenuTrigger asChild>
         <Button>Open Menu</Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="mt-3 w-60">
+      <DropdownMenuContent className="mt-3 w-115">
         {items?.map((item) => (
           <div key={item.id}>
             <DropdownMenuItem>
-              <div className="flex justify-between items-center gap-3">
+              <div className="flex justify-between items-center w-full gap-3 h-16">
                 <div>{item?.name}</div>
-                <div>
+                <div className="w-16">
                   <Image
                     src={item?.image}
                     alt={item?.name}
                     width={310}
                     height={300}
                     loading="eager"
-                    className="rounded-2xl w-full h-auto"
+                    className="rounded-2xl w-full h-auto object-cover"
                   />
                 </div>
               </div>
